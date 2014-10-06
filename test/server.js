1b3a0315f67c2aacd9d047a13ba13b354bef50d4
Lars <lars.tilsner@googlemail.com>
2014-10-06 20:51:31 +0200

    initial commit

--- test/server.js
+++ test/server.js
@@ -75,6 +75,42 @@ module.exports = {
             test.fail("Should have give an error");
         }
         test.done();
-    }
+    },
+    setInvalidObject : function(test){
+        var options = {
+                port: 'abc',
+                method: 'POST',
+                path: '/db/data/cypher?includeStats=true',
+                headers: {
+                    'Accept': 'application/json; charset=UTF-8',
+                    'Content-Type': 'application/json'
+                }
+            },
+            returnValue =server.setOptions(options);
 
+        if(! returnValue){
+            test.fail("Should have give an error");
+        }
+        test.done();
+    },
+    setOnlyCypher: function(test){
+        var object = {
+            cypher : 'MATCH N RETURN N'
+        }
+        var error = server.executeCypher(object);
+        if (error){
+            test.fail('Should not have failed');
+        }
+        test.done();
+    },
+    setCallbackCypher: function(test){
+        var object = {
+            cypher : 'MATCH N DELETE N',
+            callback : function(){
+                test.done();
+            }
+        }
+        server.executeCypher(object);
+
+    }
 }
